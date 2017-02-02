80fd10c8e93eb79ee4cf9988d94013d06b69d455
manegame <manusnijhoff@gmail.com>
2017-02-02 01:07:38 +0100

    late night commit

--- src/js/app.js
+++ src/js/app.js
@@ -11,23 +11,51 @@
     var YouTubeIframeLoader = require('youtube-iframe');
 
     var videoIds = {  movie           : 'XMM29e9X9Qc',
-                      vamos           : 'kZHu9l4nrK8',
-                      ark             : 'dyYwz4ygmQs',
-                      couch           : 'n_n4IaQYYIU',
-                      pasfoto         : '5akDcxBU2Ws',
-                      stevin          : 'EV2vlQgX6A0',
-                      mapsOverview    : 'ZvMt-RM2x8w',
-                      mapsHouse       : '8ccRcyy7Jco',
-                      mapsStreet      : 'Z3InHJ7VU9E',
-                      hague           : 'SRRw8uA0EYs',
+                      book            : '2rJZ4Tw8Ze4',
                       trailer         : 'OQUnH2m7Hbk',
-                      afsluitdijk     : 'Vwg9OamdZJk',
-                      struggle        : 'U5VG0NvcKNI'
+                      maps            : 'ZvMt-RM2x8w',
+                      maps1           : '8ccRcyy7Jco',
+                      maps2           : 'Z3InHJ7VU9E',
+                      struggle1       : 'U5VG0NvcKNI',
+                      struggle2       : 'jpVxYbuuaio',
+                      hero            : 'EmK58gR58rw',
+                      welcome         : 'vBykWs3s3Rg',
+                      welcome2        : '8IlFMzbeXTc',
+                      y2037           : 'y4zQpCmq2rg',
+                      hague           : 'SRRw8uA0EYs',
+                      vamos           : 'mWyIgNd5Z0M',
+                      water           : 'nnd9AsbAhVc',
+                      finger          : 'WDlHel9fMFY',
+                      underwater      : 'Gp_2teRctEg',
+                      ark             : 'sog9BTOums4',
+                      scenes1         : 'Ka5Lhk3g2LE',
+                      scenes2         : 'uNU4a6HBAds',
+                      scenes3         : 'n_n4IaQYYIU',
+                      scenes4         : 'AOmtkg4NyMM',
+                      scenes5         : 'TNUyJXkQjQA'
               }
 
-    var stream = [ videoIds.movie, videoIds.mapsStreet, videoIds.hague, videoIds.trailer ];
-
-    console.log(videoIds.movie);
+    var stream = [    videoIds.maps,
+                      videoIds.maps1,
+                      videoIds.maps2,
+                      videoIds.struggle1,
+                      videoIds.struggle2,
+                      videoIds.hero,
+                      videoIds.welcome,
+                      videoIds.welcome2,
+                      videoIds.y2037,
+                      videoIds.hague,
+                      videoIds.vamos,
+                      videoIds.water,
+                      videoIds.finger,
+                      videoIds.underwater,
+                      videoIds.ark,
+                      videoIds.scenes1,
+                      videoIds.scenes2,
+                      videoIds.scenes3,
+                      videoIds.scenes4,
+                      videoIds.scenes5
+                  ];
 
     var player;
     var chapterPlayer;
@@ -117,8 +145,27 @@
 
     // This function is called by initialize()
     function updateProgressBar(){
+        var value = (player.getCurrentTime() / player.getDuration()) * rangeMax;
         // Update the value of our progress bar accordingly.
-        $('#progress-bar').val((player.getCurrentTime() / player.getDuration()) * rangeMax);
+        $('#progress-bar').val(value);
+
+        // var maxHeight = $(window).height();
+        // var tooltipHeight = $('#tooltip').height();
+
+        // rangeMax = 5000
+        // value ranges from 0 to 5000
+
+        // min pixel value = (maxheight - tooltipHeight)
+        // max pixel value = 0
+
+        // 5000 / rangeMax = max pixel value
+
+        // = min pixel value
+
+        console.log(value, rangeMax);
+        $('#tooltip').css({
+          'top' : maxHeight - tooltipHeight
+        })
     }
 
     $('#play-toggle').on('click', function () {
@@ -173,7 +220,7 @@
 
         $('#player').css({
           "margin-top" : 0,
-          "margin-left" : progressBarWidth
+          "margin-left" : 0
         });
 
       }
@@ -207,13 +254,15 @@
     function openChoices() {
 
       player.loadVideoById({
-        videoId : videoIds.struggle,
+        videoId : videoIds.struggle1,
         startSeconds : menutime,
         loop : 1
       });
       player.mute();
 
       $('.choice').addClass('active').show();
+      $('#controls').hide();
+      $('#progress-bar').hide();
 
       resizeVideo();
 
@@ -246,15 +295,21 @@
 
       if (choice === 'stream') {
         player.loadPlaylist({
-                  playlist: stream
+                  playlist : stream,
+                  index    : Math.floor(Math.random() * stream.length)
                 });
       } else {
+
         player.loadVideoById({
           videoId : videoIds[choice],
         });
+
       }
 
       $('.choice').removeClass('active').hide();
+      $('#controls').show();
+      $('#progress-bar').show();
+      resizeVideo();
     })
 
 }());
